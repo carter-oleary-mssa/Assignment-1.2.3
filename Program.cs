3aa3381cfb13f0c468d52c1f31a5415a1f6d2c36
carter-oleary <carter.m.oleary@outlook.com>
2025-04-08 14:13:49 -0400

    Added comments

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
-﻿int num1 = 7;
+﻿//  Initialize two numbers
+int num1 = 7;
 int num2 = 3;
 
+// Make sure the loop runs once
 do
 {
+    // Print the menu
     Console.WriteLine("Please choose an operation to perform:");
     Console.WriteLine("1: Addition");
     Console.WriteLine("2: Subtraction");
     Console.WriteLine("3: Multiplication");
     Console.WriteLine("4: Division");
 
+    // Take user input and execute the requested operation
     int selection = Convert.ToInt32(Console.ReadLine());
     switch (selection)
     {
@@ -25,5 +29,6 @@ do
             Console.WriteLine($"{num1} / {num2} = {num1 / num2}r{num1 % num2}");
             break;
     }
+    // Check if the user wants to keep going and exit if not
     Console.WriteLine("Would you like to do another operation? (y/n)");
 } while(Console.ReadLine() != "n");
