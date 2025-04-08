f010db6207957596b533aeef212173b5f5b32968
carter-oleary-mssa <carteroleary.mssa@gmail.com>
2025-04-08 13:03:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int num1 = 7;
+int num2 = 3;
+
+while(true)
+{
+    Console.WriteLine("Please choose an operation to perform:");
+    Console.WriteLine("1: Addition");
+    Console.WriteLine("2: Subtraction");
+    Console.WriteLine("3: Multiplication");
+    Console.WriteLine("4: Division");
+
+    int selection = Convert.ToInt32(Console.ReadLine());
+    switch (selection)
+    {
+        case 1:
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            break;
+        case 2:
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            break;
+        case 3:
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            break;
+        case 4:
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}r{num1 % num2}");
+            break;
+    }
+    Console.WriteLine("Would you like to do another operation? (y/n)");
+    if(Console.ReadLine() == "n")
+    {
+        break;
+    }
+}
